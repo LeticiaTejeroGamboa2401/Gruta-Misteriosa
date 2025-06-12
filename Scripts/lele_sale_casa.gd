1d804a56791e5613fad49252ea37c69f1d39c614
LeticiaTejeroGamboa2401 <leticia.tejero.gamboa@hotmail.com>
2025-06-11 19:10:57 -0600

    corrección de direcciones y defectos

--- Scripts/lele_sale_casa.gd
+++ Scripts/lele_sale_casa.gd
@@ -6,4 +6,5 @@ func _ready() -> void:
 func _on_siguiente_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Bosque.tscn")
 	
-	
+func _on_omitir_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/mapa.tscn")

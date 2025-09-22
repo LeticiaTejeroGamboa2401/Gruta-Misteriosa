903a9162a6ecb976dc31fe3330c40f36cca7fbe1
LeticiaTejeroGamboa2401 <leticia.tejero.gamboa@hotmail.com>
2025-09-22 13:07:42 -0600

    botones de control móvil

--- Scripts/bosque_nahual.gd
+++ Scripts/bosque_nahual.gd
@@ -20,6 +20,36 @@ func _ready() -> void:
 		player_instance.speed = 200  # Velocidad personalizada
 	else:
 		print("No se ha seleccionado personaje.")
+	
+	var plataforma = OS.get_name() 
+	if plataforma == "Android" or plataforma == "iOS":
+		$TouchControls.visible = true
+	else:
+		$TouchControls.visible = false
+
+# <---- IZQUIERDA ----->
+func _on_left_button_down() -> void:
+	Input.action_press("ui_left")
+func _on_left_button_up() -> void:
+	Input.action_release("ui_left")
+
+# <---- DERECHA ----->
+func _on_right_button_down() -> void:
+	Input.action_press("ui_right")
+func _on_right_button_up() -> void:
+	Input.action_release("ui_right")
+	
+# <---- ARRIBA ----->
+func _on_up_button_down() -> void:
+	Input.action_press("ui_up")
+func _on_up_button_up() -> void:
+	Input.action_release("ui_up")
+
+# <---- ABAJO ----->
+func _on_down_button_down() -> void:
+	Input.action_press("ui_down")
+func _on_down_button_up() -> void:
+	Input.action_release("ui_down")
 
 func update_hearts():
 	$Panel/Heart1.visible = Global.lives >= 1
--- Scripts/dentro_cueva1.gd
+++ Scripts/dentro_cueva1.gd
@@ -9,3 +9,34 @@ func _ready() -> void:
 		player_instance.position = Vector2(15,520)
 	else:
 		print("No se ha seleccionado personaje.")
+	
+	var plataforma = OS.get_name() 
+	if plataforma == "Android" or plataforma == "iOS":
+		$TouchControls.visible = true
+	else:
+		$TouchControls.visible = false
+	
+	
+# <---- IZQUIERDA ----->
+func _on_left_button_down() -> void:
+	Input.action_press("ui_left")
+func _on_left_button_up() -> void:
+	Input.action_release("ui_left")
+
+# <---- DERECHA ----->
+func _on_right_button_down() -> void:
+	Input.action_press("ui_right")
+func _on_right_button_up() -> void:
+	Input.action_release("ui_right")
+	
+# <---- ARRIBA ----->
+func _on_up_button_down() -> void:
+	Input.action_press("ui_up")
+func _on_up_button_up() -> void:
+	Input.action_release("ui_up")
+
+# <---- ABAJO ----->
+func _on_down_button_down() -> void:
+	Input.action_press("ui_down")
+func _on_down_button_up() -> void:
+	Input.action_release("ui_down")
